2c6f38e1193c06bd6a7c359feb95e52f719b758a
plagentile1121@gmail.com <plagentile1121@gmail.com>
2021-07-05 22:25:50 -0500

    JWT issuing

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,47 +13,63 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext dataContext;
-        public AccountController(DataContext dataContext){
+        private readonly ITokenService itokenService;
+        public AccountController(DataContext dataContext, ITokenService itokenService)
+        {
+            this.itokenService = itokenService;
             this.dataContext = dataContext;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> RegisterUser(RegisterDto registerDto){
-            
-            if(await this.UserExists(registerDto.Username)){
+        public async Task<ActionResult<UserDto>> RegisterUser(RegisterDto registerDto)
+        {
+
+            if (await this.UserExists(registerDto.Username))
+            {
                 return BadRequest("Username is taken");
             }
 
             using var hmac = new HMACSHA512();
-            
-            var user = new AppUser{
+
+            var user = new AppUser
+            {
                 UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
-            this.dataContext.Add(user);  
+            this.dataContext.Add(user);
             await this.dataContext.SaveChangesAsync();
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                Token = this.itokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
 
             var user = await this.dataContext.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
-            if(user == null) return Unauthorized("Invalid Username or Password");
-        
-            using var hmac = new HMACSHA512(user.PasswordSalt); 
+            if (user == null) return Unauthorized("Invalid Username or Password");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for(int x =0; x < computedHash.Length; x++){
-                if(computedHash[x] != user.PasswordHash[x]) return Unauthorized("Invalid Username or Password");
+            for (int x = 0; x < computedHash.Length; x++)
+            {
+                if (computedHash[x] != user.PasswordHash[x]) return Unauthorized("Invalid Username or Password");
             }
-            return user;
+            
+            return new UserDto{
+                Username = user.UserName,
+                Token = this.itokenService.CreateToken(user)
+            };
         }
 
-        private async Task<bool> UserExists(string username){
-            return await this.dataContext.Users.AnyAsync(user =>  user.UserName == username.ToLower());
+        private async Task<bool> UserExists(string username)
+        {
+            return await this.dataContext.Users.AnyAsync(user => user.UserName == username.ToLower());
         }
 
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -29,6 +23,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService, TokenService>();
+
             services.AddDbContext<DataContext>(options => 
             {
                 options.UseSqlite(this.iconfig.GetConnectionString("DefaultConnection"));
