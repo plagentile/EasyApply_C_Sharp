8191a142f77900ff557ee72a7df719516a937653
plagentile1121@gmail.com <plagentile1121@gmail.com>
2021-07-07 22:42:12 -0500

    Angular Navibar component

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,16 +1,10 @@
-using System.Text;
-using API.Data;
+
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 
 namespace API
 {
