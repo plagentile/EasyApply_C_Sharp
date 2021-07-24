a5cfc7449120bd5bee749d3a6b6ef716b53705f9
plagentile1121@gmail.com <plagentile1121@gmail.com>
2021-07-24 16:57:44 -0500

    Update account.service.ts

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -20,11 +20,11 @@ export class AccountService {
        map((response : User) => {
          const user = response;
          if(user){
-           console.log(user);
            localStorage.setItem('user', JSON.stringify(user));
+           this.currentUserSource.next(user);
          }
        })
-     );
+     )
    }
 
    setCurrentUser(user : User){
