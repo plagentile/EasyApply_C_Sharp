707221f9529d0cea6f6e2543c5b4e4149ca7a872
plagentile1121@gmail.com <plagentile1121@gmail.com>
2021-07-25 23:44:32 -0500

    routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './info/about/about.component';
+import { TosComponent } from './info/tos/tos.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'tos', component:TosComponent},
   {path: '**', component:HomeComponent, pathMatch: 'full'} //not found, wildcard
 ];
 
